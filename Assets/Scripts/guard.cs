f3c3ff3c1d5c51bf9d3f81f886d3365ba570347b
Haley Shalam <hs2590@nyu.edu>
2014-08-04 13:30:55 -0400

    added tags

---
+++ Assets/Scripts/guard.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class guard : MonoBehaviour {
+
+	//bring in variables
+	public int direction = 0;
+	
+	void Update () {
+		// move forward
+			
+		// strafe left
+
+			//if not facing left just change players direction
+			if (direction != 2)
+			{
+				transform.rotation = Quaternion.Euler(0, 270, 0);
+				
+				//set the direction to left
+				direction = 2;
+			}
+			//otherwise move left
+			else
+			{
+				transform.position += new Vector3 (0, 0, 1);
+			}
+			
+			
+
+		// strafe right
+		if (Input.GetKeyDown (KeyCode.RightArrow)) {
+			
+			//if not facing right just change players direction
+			if (direction != 3)
+			{
+				transform.rotation = Quaternion.Euler(0, 90, 0);
+				
+				//set the direction to right
+				direction = 3;
+			}
+			//otherwise move left
+			else
+			{
+				transform.position += new Vector3 (0, 0, -1);
+			}
+			
+			
+		}
+		
+		
+		
+	}
+}
