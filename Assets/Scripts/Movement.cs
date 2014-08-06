68e3845a080abf0d3fa10eaf72bc2b1d45492a80
Maurice Shalam <mss619@nyu.edu>
2014-08-06 16:01:18 -0400

    added trigger box to the player to detect obstacles. however the trigger box only work sometimes (which is weird and i am trying to fix)

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,8 +5,11 @@ public class Movement : MonoBehaviour {
 	
 	//bring in variables
 	public int direction = 0;
+	public bool canMove = true;
+	public int obstacleDir = -1;
 	
 	void Update () {
+
 		// move forward
 		if ( Input.GetKeyDown (KeyCode.UpArrow) ) {
 			
@@ -21,13 +24,20 @@ public class Movement : MonoBehaviour {
 			//otherwise move forward
 			else
 			{
+				//if you can move forward than do it
+				if (obstacleDir != 0)
+				{
+
 				transform.position += new Vector3 (1, 0, 0);
 				Camera.main.transform.position += new Vector3 (1, 0, 0);
+
+				}
+
 			}
 			
 		}
 		// move backward
-		if ( Input.GetKeyDown (KeyCode.DownArrow) ) {
+		if ( Input.GetKeyDown (KeyCode.DownArrow)  ) {
 			
 			//if not facing back just change players direction
 			if (direction != 1)
@@ -40,13 +50,18 @@ public class Movement : MonoBehaviour {
 			//otherwise move back
 			else
 			{
+				//if you can move back than do it
+				if (obstacleDir != 1)
+				{
 				transform.position += new Vector3 (-1, 0, 0);
 				Camera.main.transform.position += new Vector3 (-1, 0, 0);
+				}
+
 			}
 			
 		}
 		// strafe left
-		if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+		if (Input.GetKeyDown (KeyCode.LeftArrow) ) {
 			
 			//if not facing left just change players direction
 			if (direction != 2)
@@ -59,14 +74,18 @@ public class Movement : MonoBehaviour {
 			//otherwise move left
 			else
 			{
+				//if you can move left than do it
+				if (obstacleDir != 2)
+				{
 				transform.position += new Vector3 (0, 0, 1);
 				Camera.main.transform.position += new Vector3 (0, 0, 1);
+				}
 			}
 			
 			
 		}
 		// strafe right
-		if (Input.GetKeyDown (KeyCode.RightArrow)) {
+		if (Input.GetKeyDown (KeyCode.RightArrow) ) {
 			
 			//if not facing right just change players direction
 			if (direction != 3)
@@ -76,11 +95,16 @@ public class Movement : MonoBehaviour {
 				//set the direction to right
 				direction = 3;
 			}
-			//otherwise move left
+			//otherwise move right
 			else
 			{
+				//if you can move right than do it
+				if (obstacleDir != 3)
+				{
 				transform.position += new Vector3 (0, 0, -1);
 				Camera.main.transform.position += new Vector3 (0, 0, -1);
+				}
+
 			}
 			
 			
@@ -89,4 +113,50 @@ public class Movement : MonoBehaviour {
 		
 		
 	}
+
+	// You will need a trigger-collider on this object
+	void OnTriggerEnter (Collider other ) {		
+		
+	
+
+		Debug.Log("entered trigger");
+		Debug.Log(other.gameObject.tag);
+		if (other.gameObject.tag == "Obstacle")
+		{
+			Debug.Log("hit obstacle");
+			//canMove = false;
+			obstacleDir = direction;
+		}
+		
+	}
+
+	void OnTriggerStay(Collider other2)
+	{
+		/*
+		if (other2.gameObject.tag == "Obstacle")
+		{
+			Debug.Log("hit trigger stay obstacle");
+			//canMove = false;
+			obstacleDir = direction;
+		}
+		*/
+
+	}
+
+	
+	//performs actions upon exiting trigger box
+	void OnTriggerExit()
+	{
+
+
+		//canMove = true;
+		obstacleDir = -1;
+		Debug.Log("exited trigger");
+
+
+	}
+
+
+
+
 }
