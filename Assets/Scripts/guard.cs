3839434d5f4397c340b1edde895e136dda34aca5
Haley Shalam <hs2590@nyu.edu>
2014-08-07 15:46:01 -0400

    added movement and rudimentary ray casting to guard

--- Assets/Scripts/guard.cs
+++ Assets/Scripts/guard.cs
@@ -6,6 +6,20 @@ public class guard : MonoBehaviour {
 	//bring in variables
 	public int direction = 1;
 	public float move = 1f;
+
+	/*
+	void OnCollisionEnter(Collision other)
+	{
+		Debug.Log("collision detected");
+		if (other.gameObject.tag != "Player")
+		{
+			//move *= -1;
+			direction *= -1;
+			Debug.Log("changed the players direction");
+		}
+
+		
+	}*/
 	
 	void Update () {
 
@@ -17,7 +31,6 @@ public class guard : MonoBehaviour {
 				transform.rotation = Quaternion.Euler(0, 270, 0);
 				//Debug.Log("rotated the player left");
 				//set the direction to nothing, so this isn't called every frame
-				direction = 0;
 			}
 
 			if (direction == 1)
@@ -25,7 +38,6 @@ public class guard : MonoBehaviour {
 				transform.rotation = Quaternion.Euler(0, 90, 0);
 				//Debug.Log("rotated the player right");
 				//set the direction to nothing, so this isn't called every frame
-				direction = 0;
 			}
 
 
@@ -34,25 +46,35 @@ public class guard : MonoBehaviour {
 			//Debug.Log("moved the player");
 			//Debug.Log(transform.position);
 			
+			//raycast to detect walls
+			Vector3 fwd = transform.TransformDirection(Vector3.forward);
+			if (Physics.Raycast(transform.position, fwd, 2))
+			{
+				print("There is something in front of the object!");	
+				move *= -1;
+				direction *= -1;
+				Debug.Log(direction);
+			}
 
-		
-		
-		
+			RaycastHit rayHit = new RaycastHit(); // empty, blank
+
+			//raycasts to detect players
+		if (Physics.Raycast(transform.position, fwd, out rayHit, 10))
+			{
+				
+				
+				if(rayHit.transform.gameObject.tag == "Player")
+				{	
+					print("hit player");
+					Destroy ( rayHit.transform.gameObject );
+				}
+
+			}
+
 	}
 
-	//if it collides into something change the direction
-	void OnCollisionEnter(Collision other)
-	{
-		Debug.Log("collision detected");
-		if (other.gameObject.tag != "Player")
-		{
-			//move *= -1;
-			direction *= -1;
-			Debug.Log("changed the players direction");
-		}
 
 
-	}
 
 
 
