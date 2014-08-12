3f88556cba350ce95f5543f88b80e6886103d70c
fc1159 <fc1159@nyu.edu>
2014-08-12 16:28:02 -0400

    Guards and cameras fully operational

--- Assets/Scripts/SpotlightCollison.cs
+++ Assets/Scripts/SpotlightCollison.cs
@@ -2,21 +2,36 @@
 using System.Collections;
 
 public class SpotlightCollison : MonoBehaviour {
-
+	
+	Vector3 fwd;
+	
+	public float degreeOffset;
+	public float spotRange;
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		//Raycast to spot player
-		Vector3 fwd = transform.TransformDirection(Vector3.forward);
+		
+		if( transform.forward.z > 0f )
+		{
+			fwd = Quaternion.Euler(degreeOffset, 0, 0) * transform.forward;
+		}
+		else
+		{
+			fwd = Quaternion.Euler(degreeOffset * -1f, 0, 0) * transform.forward; 
+		}
+		
 		RaycastHit rayHit = new RaycastHit();
 		
-		if (Physics.Raycast(transform.position, fwd, out rayHit, Mathf.Infinity)) {
+		Debug.DrawLine (transform.position, transform.position + fwd);
+		
+		if (Physics.Raycast(transform.position, fwd, out rayHit, spotRange)) {
 			if (rayHit.collider.tag == "Player") {
-				Application.LoadLevel(2);
+				Application.LoadLevel(0);
 			}
 		}
 	}
