6bb4f7950b91ab5084a6a952d726c2ed10b49bba
fc1159 <fc1159@nyu.edu>
2014-08-12 13:09:53 -0400

    First Playtest Push

---
+++ Assets/Scripts/SpotlightCollison.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpotlightCollison : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Raycast to spot player
+		Vector3 fwd = transform.TransformDirection(Vector3.forward);
+		RaycastHit rayHit = new RaycastHit();
+		
+		if (Physics.Raycast(transform.position, fwd, out rayHit, Mathf.Infinity)) {
+			if (rayHit.collider.tag == "Player") {
+				Application.LoadLevel(0);
+			}
+		}
+	}
+}
